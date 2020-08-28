d9ea8596b626e0f94803fb0cb76fe82db8a8e652
HristovCodes <ivailohristov1990@gmail.com>
2020-08-28 13:36:53 +0300

    Styled register page. Mobile only atm

--- events-board/src/components/Register/index.tsx
+++ events-board/src/components/Register/index.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style.scss";
+import Logo from "../media/logo.png";
 
 export default function Register() {
   return (
-    <div className="signin">
-      <h1>This is register</h1>
-      <Link to="/register">Sign Up</Link>
-      <Link to="/login">Sign In</Link>
-      <Link to="/">Continue as guest</Link>
+    <div className="register">
+      <form className="regform">
+        <h1>Eventó Board</h1>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input aria-required="true" type="text" name="username"></input>
+        </div>
+        <div>
+          <label htmlFor="email">E-mail:</label>
+          <input aria-required="true" type="email" name="email"></input>
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input aria-required="true" type="password" name="password"></input>
+        </div>
+        <Link to="/">Sign Up</Link>
+        <Link to="/login">Sign In</Link>
+        <img src={Logo} alt="logo"></img>
+      </form>
     </div>
   );
 }
