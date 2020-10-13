5ff0fd2145b188c62304bffa4126c1c4a9e820cd
HristovCodes <ivailohristov1990@gmail.com>
2020-10-13 22:42:32 +0300

    Decluttered home component now only acts as a router

--- events-board/src/components/Home/index.tsx
+++ events-board/src/components/Home/index.tsx
@@ -1,10 +1,6 @@
+// eslint-disable-next-line
 import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LogIn from "../LogIn/index";
 import Feed from "../Feed/index";
 import Register from "../Register/index";
@@ -13,31 +9,20 @@ import "./style.scss";
 
 export default function Home() {
   const [open, openSB] = useState(false);
-  const [userIsAuthenticated, authenticate] = useState(false);
+
   return (
     <Router>
-      {/* This is the sidebar */}
       <div className="wrapper">
         <SideBar openSB={openSB} open={open}></SideBar>
         <Switch>
           <Route exact path="/">
-            {!userIsAuthenticated ? (
-              <Redirect to="/register"></Redirect>
-            ) : (
-              <Feed onClick={openSB} />
-            )}
+            <Feed onClick={openSB} />
           </Route>
           <Route path="/login">
-            <LogIn
-              authUser={authenticate}
-              isAuthenticated={userIsAuthenticated}
-            />
+            <LogIn />
           </Route>
           <Route path="/register">
-            <Register
-              authUser={authenticate}
-              isAuthenticated={userIsAuthenticated}
-            />
+            <Register />
           </Route>
         </Switch>
       </div>
