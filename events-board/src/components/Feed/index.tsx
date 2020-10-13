bd1dc4cf5a44c294c023ecae77c58e19de079899
HristovCodes <ivailohristov1990@gmail.com>
2020-10-13 22:41:04 +0300

    Feed redirects to "/login" if not signed in

--- events-board/src/components/Feed/index.tsx
+++ events-board/src/components/Feed/index.tsx
@@ -1,16 +1,42 @@
-import React from "react";
+// eslint-disable-next-line
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import Swipe from "../Swipe/index";
 import Firebase from "../../firebase";
-import { stringify } from "querystring";
+import { Redirect, useHistory } from "react-router-dom";
 
 interface FeedProps {
   onClick: any;
 }
 
 export default function Feed({ onClick }: FeedProps) {
-  let user = Firebase.auth.currentUser;
-  return (
+  const [isAuth, auth] = useState(true);
+  let history = useHistory();
+
+  let signOut = function () {
+    Firebase.auth
+      .signOut()
+      .then(() => {
+        history.replace("/login");
+      })
+      .catch(function (error) {
+        // An error happened.
+        alert(error.code);
+        alert(error.message);
+      });
+  };
+
+  useEffect(() => {
+    Firebase.auth.onAuthStateChanged(function (user) {
+      if (user) {
+        auth(true);
+      } else {
+        auth(false);
+      }
+    });
+  });
+
+  return isAuth ? (
     <main>
       <Swipe
         touchEnd={() => {
@@ -19,9 +45,11 @@ export default function Feed({ onClick }: FeedProps) {
       ></Swipe>
       <div className="home" onClick={() => onClick(false)}>
         <h1>This is home</h1>
-        <p>{user?.email}</p>
+        <button onClick={signOut}>sign out</button>
       </div>
       <button className="sbbtn" onClick={() => onClick(true)}></button>
     </main>
+  ) : (
+    <Redirect to="/login"></Redirect>
   );
 }
