76e08e87b20ea2efd027b9640141d93285701b94
HristovCodes <ivailohristov1990@gmail.com>
2020-10-17 18:17:52 +0300

    Moved signout to sidebar component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,14 +1,32 @@
 // eslint-disable-next-line
 import React from "react";
 import "./style.scss";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import Firebase from "../../firebase";
 
 interface SideBarProps {
   open: boolean;
   openSB: any;
+  auth: any;
 }
 
-export default function SideBar({ open, openSB }: SideBarProps) {
+export default function SideBar({ open, openSB, auth }: SideBarProps) {
+  let history = useHistory();
+
+  let signOut = function () {
+    Firebase.auth
+      .signOut()
+      .then(() => {
+        auth(false);
+        history.replace("/login");
+      })
+      .catch(function (error) {
+        // An error happened.
+        alert(error.code);
+        alert(error.message);
+      });
+  };
+
   return (
     <aside className={"sidebar " + open.toString()}>
       <ul>
@@ -21,6 +39,9 @@ export default function SideBar({ open, openSB }: SideBarProps) {
           >
             Home
           </Link>
+        </li>
+        <li>
+          <a onClick={signOut}>Log off</a>
         </li>
       </ul>
     </aside>
