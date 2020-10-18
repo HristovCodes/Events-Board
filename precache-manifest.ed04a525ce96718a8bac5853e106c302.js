92088fd8d485b8b4b1d31f512a0593dbaaeec34e
HristovCodes <ivailohristov1990@gmail.com>
2020-10-18 12:11:40 +0300

    Updates

---
+++ precache-manifest.ed04a525ce96718a8bac5853e106c302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca93488d6d66dcaf4481e346d7d2fa66",
+    "url": "/Events-Board/index.html"
+  },
+  {
+    "revision": "c0b6dd1adfa31e1757ac",
+    "url": "/Events-Board/static/css/main.697fc332.chunk.css"
+  },
+  {
+    "revision": "49c70dad5678d482bcf4",
+    "url": "/Events-Board/static/js/2.55304f9d.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/Events-Board/static/js/2.55304f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b6dd1adfa31e1757ac",
+    "url": "/Events-Board/static/js/main.ded4c9ad.chunk.js"
+  },
+  {
+    "revision": "68163fb889f6636e8017",
+    "url": "/Events-Board/static/js/runtime-main.3bbe8b15.js"
+  }
+]);
