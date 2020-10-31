cfa9c4c46fb5d5db094f70101547aabaeacf1e8d
Bozhidar-A <bplatanasov@gmail.com>
2020-10-31 16:40:41 +0200

    do not use <form> with <button>

--- src/components/ChangeProfileData/index.tsx
+++ src/components/ChangeProfileData/index.tsx
@@ -22,110 +22,109 @@ export default function ChangeProfileData({
 
   const query = new URLSearchParams(window.location.search);
   const varToChange = query.get('v')
-  
-  
+
+
   let history = useHistory();
 
-  let updateEmail = function(){
+  let updateEmail = () => {
     var user = Firebase.auth.currentUser;
 
-    if(user){//user exists
+    if (user) {//user exists
       user.updateEmail(email)
-          .then(() => {//update email
-            alert("Your Email has been changed!");
-            Firebase.auth.signOut().then(() => {
-              auth(false);//set auth to false
-              history.replace("/Events-Board/LogIn");
-            })//sign out
+        .then(() => {//update email
+          alert("Your Email has been changed!");
+          Firebase.auth.signOut().then(() => {
+            auth(false);//set auth to false
+            history.replace("/Events-Board/LogIn");
+          })//sign out
             .catch(function (error) {
               // An error happened.
               alert(error.code);
               alert(error.message);
             });
-          }).catch(function (error){
-            alert(error.code);
-            alert(error.message);//something happened
+        }).catch(function (error) {
+          alert(error.code);
+          alert(error.message);//something happened
 
-            if(error.code === "auth/requires-recent-login")//user has been logged in for too long relog
-            {
-              Firebase.auth.signOut().then(() => {
-                auth(false);//set auth to false
-                history.replace("/Events-Board/LogIn");
-              })//sign out
+          if (error.code === "auth/requires-recent-login")//user has been logged in for too long relog
+          {
+            Firebase.auth.signOut().then(() => {
+              auth(false);//set auth to false
+              history.replace("/Events-Board/LogIn");
+            })//sign out
               .catch(function (error) {
                 // An error happened.
                 alert(error.code);
                 alert(error.message);
               });
-            }
-          })
+          }
+        })
     }
   }
 
-  let updatePhotoURL = function(){
+  let updatePhotoURL = function () {
     var user = Firebase.auth.currentUser;
 
-    if(user && validPicURL){//user exists and photoURL is valid
-      user.updateProfile({photoURL : photoURL})//update it
+    if (user && validPicURL) {//user exists and photoURL is valid
+      user.updateProfile({ photoURL: photoURL })//update it
         .then(() => {
           alert("Your Picture has been updated!")
           history.replace("/Events-Board");//bring to main no reason to relog
-        }).catch(function (error){//something happened
+        }).catch(function (error) {//something happened
           alert(error.code);
           alert(error.message);
         })
     }
   }
 
-  let updatePassword = function(){
+  let updatePassword = function () {
     var user = Firebase.auth.currentUser;
 
-    if(user){//user exists
-      if(password === confirmPassword)//password and confirm_password fields have the same value
+    if (user) {//user exists
+      if (password === confirmPassword)//password and confirm_password fields have the same value
       {
         user.updatePassword(password)
-        .then(() => {//update and logout
-          alert("Your Password has been changed!");
-          Firebase.auth.signOut().then(() => {
+          .then(() => {//update and logout
+            alert("Your Password has been changed!");
+            Firebase.auth.signOut().then(() => {
               auth(false);//set auth to false
               history.replace("/Events-Board/LogIn");
             })//sign out
-            .catch(function (error) {
-              // An error happened.
-              alert(error.code);
-              alert(error.message);
-            });
-        }).catch(function (error){//something happened
-          alert(error.code);
-          alert(error.message);
+              .catch(function (error) {
+                // An error happened.
+                alert(error.code);
+                alert(error.message);
+              });
+          }).catch(function (error) {//something happened
+            alert(error.code);
+            alert(error.message);
 
-          if(error.code === "auth/requires-recent-login")//user has been logged in for too long relog
-          {
-            Firebase.auth.signOut();
-            auth(false);
-            history.replace("/Events-Board/LogIn");
-          }
-        })
+            if (error.code === "auth/requires-recent-login")//user has been logged in for too long relog
+            {
+              Firebase.auth.signOut();
+              auth(false);
+              history.replace("/Events-Board/LogIn");
+            }
+          })
       }
-      else{//adding this ugly if just for this message
+      else {//adding this ugly if just for this message
         alert("Both fields MUST match!");
       }
     }
   }
 
   let htmlToRender;
-  if(varToChange === "Email")//check if email prep form
+  if (varToChange === "Email")//check if email prep form
   {
     htmlToRender =
-    <div>
-      <Swipe
+      <div>
+        <Swipe
           touchEnd={() => {
             onClick(true);
           }}
-      ></Swipe>
-      <a className="sbbtn" onClick={() => onClick(true)}></a>
-      <form className="emailform">
-        <h1>Please enter your new Email.</h1>
+        ></Swipe>
+        <a className="sbbtn" onClick={() => onClick(true)}></a>
+          <h1>Please enter your new Email.</h1>
           <label htmlFor="email">E-mail:</label>
           <input
             aria-required="true"
@@ -134,17 +133,16 @@ export default function ChangeProfileData({
             id="email"
             onChange={(e) => setEmail(e.target.value)}
           ></input>
-        <a className="btnmain" onClick={updateEmail}>
-          Update Email
-        </a>
-      </form>
-    </div>
-    
+          <button className="btnmain" onClick={updateEmail}>
+            Update Email
+        </button>
+      </div>
+
   }
 
-  if(varToChange === "Photo")//check if photo and prep form
+  if (varToChange === "Photo")//check if photo and prep form
   {
-      htmlToRender = 
+    htmlToRender =
       <div>
         <Swipe
           touchEnd={() => {
@@ -152,8 +150,7 @@ export default function ChangeProfileData({
           }}
         ></Swipe>
         <a className="sbbtn" onClick={() => onClick(true)}></a>
-        <form className="photoform">
-        <label htmlFor="photo">Photo URL:</label>
+          <label htmlFor="photo">Photo URL:</label>
           <input
             aria-required="true"
             type="input"
@@ -161,68 +158,65 @@ export default function ChangeProfileData({
             id="photo"
             onChange={(e) => setPhotoURL(e.target.value)}
           ></input>
-        <div>
-          <p>Live Preview</p>
-          <br></br>
-          <div className="imgPreview">
-           <img src={photoURL} alt="The link provided is not valid" onLoad={() => setValidity(true)} onError={() => setValidity(false)} ></img>
+          <div>
+            <p>Live Preview</p>
+            <br></br>
+            <div className="imgPreview">
+              <img src={photoURL} alt="The link provided is not valid" onLoad={() => setValidity(true)} onError={() => setValidity(false)} ></img>
+            </div>
           </div>
-        </div>
-        <a className="btnmain" onClick={updatePhotoURL} >
-          Update Photo
-        </a>
-        </form>
+          <button className="btnmain" onClick={updatePhotoURL} >
+            Update Photo
+        </button>
       </div>
-      
+
   }
 
-  if(varToChange === "Password")//check if password and prep form
+  if (varToChange === "Password")//check if password and prep form
   {
     htmlToRender =
-    <div>
-      <Swipe
+      <div>
+        <Swipe
           touchEnd={() => {
             onClick(true);
           }}
-      ></Swipe>
-      <a className="sbbtn" onClick={() => onClick(true)}></a>
-      <form className="passwordform">
-      <h1>Please enter your new Password.</h1>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          aria-required="true"
-          pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-          type="password"
-          name="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <br></br>
-        <label htmlFor="password">Confirm Password:</label>
-        <input
-          aria-required="true"
-          pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-          type="password"
-          name="confirm_password"
-          id="confirm_password"
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        ></input>
+        ></Swipe>
+        <a className="sbbtn" onClick={() => onClick(true)}></a>
+          <h1>Please enter your new Password.</h1>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              aria-required="true"
+              pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <br></br>
+            <label htmlFor="password">Confirm Password:</label>
+            <input
+              aria-required="true"
+              pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+              type="password"
+              name="confirm_password"
+              id="confirm_password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></input>
+          </div>
+          <button className="btnmain" onClick={updatePassword}>
+            Update Password
+      </button>
       </div>
-      <a className="btnmain" onClick={updatePassword}>
-        Update Password
-      </a>
-      </form>
-    </div>
-    
+
   }
 
-  
+
 
   return (
-      <main >
-        {/* render needed form */}
-        {htmlToRender} 
-      </main>
+    <main >
+      {/* render needed form */}
+      {htmlToRender}
+    </main>
   );
 }
