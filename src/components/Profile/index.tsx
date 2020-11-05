f8d4ecaee8a96f92c84263dd8bfe3c13becbafe3
HristovCodes <ivailohristov1990@gmail.com>
2020-11-05 19:28:07 +0200

    Changes to page structure (prep for styling)

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line
 import React from "react";
 import "./style.scss";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Swipe from "../Swipe";
 
 interface ProfileProps {
@@ -9,36 +9,60 @@ interface ProfileProps {
   onClick: any;
 }
 
-export default function Profile({
-  userData,
-  onClick
-}: ProfileProps) {
-  let history = useHistory();
-
+export default function Profile({ userData, onClick }: ProfileProps) {
   return (
-      <main className="main">
-          <Swipe
-            touchEnd={() => {
-              onClick(true);
-            }}
-          ></Swipe>
-          <a className="sbbtn" onClick={() => onClick(true)}></a>
-          <div>
-          <p>Display name: {userData?.displayName}</p>
-          <br></br>
-          <p>Email: {userData?.email}</p>
-          <Link to={{pathname:"/Events-Board/ChangeProfileData", search:"v=Email"}}>Update Email</Link>
-          <br></br>
-          <br></br>
-          <img alt="No Photo URL provided" src={userData?.photoURL}></img>
-          <br></br>
-          <Link to={{pathname:"/Events-Board/ChangeProfileData", search:"v=Photo"}}>Update Photo</Link>
-          <br></br>
-          <br></br>
-            {userData?.emailVerified ? <p>Your Email is verified!</p> : <p>Your Email has NOT been verified. <br></br>Please verify it.</p>}
-          <br></br>
-          <Link to={{pathname:"/Events-Board/ChangeProfileData", search:"v=Password"}}>Update Password</Link>
+    <main className="main">
+      <Swipe
+        touchEnd={() => {
+          onClick(true);
+        }}
+      ></Swipe>
+      <a className="sbbtn" onClick={() => onClick(true)}></a>
+      <div className="profile" onClick={() => onClick(false)}>
+        <div>
+          <h2>Display name:</h2>
+          <p>{userData?.displayName}</p>
         </div>
-      </main>
+        <div>
+          <div>
+            <h2> Email: </h2>
+            <Link
+              to={{
+                pathname: "/Events-Board/ChangeProfileData",
+                search: "v=Email",
+              }}
+            >
+              Update Email
+            </Link>
+          </div>
+          <p>
+            {userData?.email} (
+            {userData?.emailVerified ? (
+              <span>verified</span>
+            ) : (
+              <span>not verified</span>
+            )}
+            )
+          </p>
+        </div>
+        <img alt="No Photo URL provided" src={userData?.photoURL}></img>
+        <Link
+          to={{
+            pathname: "/Events-Board/ChangeProfileData",
+            search: "v=Photo",
+          }}
+        >
+          Update Photo
+        </Link>
+        <Link
+          to={{
+            pathname: "/Events-Board/ChangeProfileData",
+            search: "v=Password",
+          }}
+        >
+          Update Password
+        </Link>
+      </div>
+    </main>
   );
 }
