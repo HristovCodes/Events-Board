db8363dd86487f8feb4c963d18cbd224a777e804
HristovCodes <ivailohristov1990@gmail.com>
2020-11-05 19:24:02 +0200

    Wrapper component for all pages

---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line
+import React from "react";
+import Swipe from "../Swipe";
+
+interface WrapperProps {
+  onClick: any;
+  cssClass: string;
+  children: any;
+}
+
+export default function Wrapper({ onClick, cssClass, children }: WrapperProps) {
+  return (
+    <main className="main">
+      <Swipe
+        touchEnd={() => {
+          onClick(true);
+        }}
+      ></Swipe>
+      <a className="sbbtn" onClick={() => onClick(true)}></a>
+      <div className={cssClass} onClick={() => onClick(false)}>
+        {children}
+      </div>
+    </main>
+  );
+}
