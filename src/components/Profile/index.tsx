6c469ebcd90471cad6dfad69c04e273fd8cf7dc6
HristovCodes <ivailohristov1990@gmail.com>
2020-11-05 19:54:05 +0200

    Wrapper to Profile compoent

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./style.scss";
 import { Link } from "react-router-dom";
 import Swipe from "../Swipe";
+import Wrapper from "../Wrapper";
 
 interface ProfileProps {
   userData: any;
@@ -11,58 +12,50 @@ interface ProfileProps {
 
 export default function Profile({ userData, onClick }: ProfileProps) {
   return (
-    <main className="main">
-      <Swipe
-        touchEnd={() => {
-          onClick(true);
-        }}
-      ></Swipe>
-      <a className="sbbtn" onClick={() => onClick(true)}></a>
-      <div className="profile" onClick={() => onClick(false)}>
-        <div>
-          <h2>Display name:</h2>
-          <p>{userData?.displayName}</p>
-        </div>
-        <div>
-          <div>
-            <h2> Email: </h2>
-            <Link
-              to={{
-                pathname: "/Events-Board/ChangeProfileData",
-                search: "v=Email",
-              }}
-            >
-              Update Email
-            </Link>
-          </div>
-          <p>
-            {userData?.email} (
-            {userData?.emailVerified ? (
-              <span>verified</span>
-            ) : (
-              <span>not verified</span>
-            )}
-            )
-          </p>
-        </div>
-        <img alt="No Photo URL provided" src={userData?.photoURL}></img>
-        <Link
-          to={{
-            pathname: "/Events-Board/ChangeProfileData",
-            search: "v=Photo",
-          }}
-        >
-          Update Photo
-        </Link>
-        <Link
-          to={{
-            pathname: "/Events-Board/ChangeProfileData",
-            search: "v=Password",
-          }}
-        >
-          Update Password
-        </Link>
+    <Wrapper onClick={onClick} cssClass="profile">
+      <div>
+        <h2>Display name:</h2>
+        <p>{userData?.displayName}</p>
       </div>
-    </main>
+      <div>
+        <div>
+          <h2> Email: </h2>
+          <Link
+            to={{
+              pathname: "/Events-Board/ChangeProfileData",
+              search: "v=Email",
+            }}
+          >
+            Update Email
+          </Link>
+        </div>
+        <p>
+          {userData?.email} (
+          {userData?.emailVerified ? (
+            <span>verified</span>
+          ) : (
+            <span>not verified</span>
+          )}
+          )
+        </p>
+      </div>
+      <img alt="No Photo URL provided" src={userData?.photoURL}></img>
+      <Link
+        to={{
+          pathname: "/Events-Board/ChangeProfileData",
+          search: "v=Photo",
+        }}
+      >
+        Update Photo
+      </Link>
+      <Link
+        to={{
+          pathname: "/Events-Board/ChangeProfileData",
+          search: "v=Password",
+        }}
+      >
+        Update Password
+      </Link>
+    </Wrapper>
   );
 }
