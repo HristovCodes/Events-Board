b627fa26a22f368b8128b9796e3ac59846111011
HristovCodes <ivailohristov1990@gmail.com>
2020-11-16 19:31:23 +0200

    Implemented searching by date and only show future events

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -9,9 +9,27 @@ interface FeedProps {
   onClick: any;
 }
 
+function isDateAfterToday(date: string) {
+  // today
+  let today = new Date();
+  today.setFullYear(today.getFullYear());
+  today.setDate(today.getDate());
+  today.setMonth(today.getMonth());
+  // date of the event
+  let dateyyyy = +date.substring(0, 4);
+  let datemm = +date.substring(5, 7) - 1;
+  let datedd = +date.substring(8, 10);
+  let temp = new Date(dateyyyy, datemm, datedd);
+  if (temp < today) {
+    return false;
+  }
+  return true;
+}
+
 export default function Feed({ onClick }: FeedProps) {
   const [events, setEvents] = useState();
   const [attendees, setAttendees] = useState();
+  const [search, setSearch] = useState("");
 
   // single call to the database to get both events and attendees
   let getData = () => {
@@ -27,6 +45,7 @@ export default function Feed({ onClick }: FeedProps) {
         });
   };
 
+  // returns all events past today's date
   let structureEvents = (
     eventData: [Object] | undefined,
     attendeesData: [Object] | undefined
@@ -35,39 +54,96 @@ export default function Feed({ onClick }: FeedProps) {
       let temp = [];
 
       for (const valuea of Object.entries(eventData)) {
-        for (const valueb of Object.entries(valuea[1])) {
-          // valueb[0] is the unique key
-          let att = {
-            g: 0,
-            i: 0,
-          };
-          for (const [key, val] of Object.entries(attendeesData)) {
-            if (key === valueb[0]) {
-              for (const x of Object.entries(val)) {
-                if (x[0] === "i") {
-                  att.i = Object.entries(x[1]).length;
-                } else {
-                  att.g = Object.entries(x[1]).length;
+        if (isDateAfterToday(valuea[0]))
+          for (const valueb of Object.entries(valuea[1])) {
+            // valueb[0] is the unique key
+            let att = {
+              g: 0,
+              i: 0,
+            };
+            for (const [key, val] of Object.entries(attendeesData)) {
+              if (key === valueb[0]) {
+                for (const x of Object.entries(val)) {
+                  if (x[0] === "i") {
+                    att.i = Object.entries(x[1]).length;
+                  } else {
+                    att.g = Object.entries(x[1]).length;
+                  }
                 }
               }
             }
-          }
 
-          temp.push({
-            0: valueb[0],
-            1: (
-              <Event
-                date={valueb[1].eventDate}
-                name={valueb[1].eventTitle}
-                description={valueb[1].eventDesc}
-                ammountGoing={att.g}
-                ammountInterested={att.i}
-                url={valueb[1].eventUrl}
-                id={valueb[0]}
-              ></Event>
-            ),
-          });
-        }
+            temp.push({
+              0: valueb[0],
+              1: (
+                <Event
+                  date={valueb[1].eventDate}
+                  name={valueb[1].eventTitle}
+                  description={valueb[1].eventDesc}
+                  ammountGoing={att.g}
+                  ammountInterested={att.i}
+                  url={valueb[1].eventUrl}
+                  id={valueb[0]}
+                ></Event>
+              ),
+            });
+          }
+      }
+      let wrapper = (
+        <ul className="eventsGrid">
+          {temp.map((el) => {
+            return <li key={el[0]}>{el[1]}</li>;
+          })}
+        </ul>
+      );
+      return wrapper;
+    }
+    return <p>"Loading..."</p>;
+  };
+
+  // only returns events that match the date searched for
+  let eventsForDate = (
+    eventData: [Object] | undefined,
+    attendeesData: [Object] | undefined
+  ) => {
+    if (eventData !== undefined && attendeesData !== undefined) {
+      let temp = [];
+
+      for (const valuea of Object.entries(eventData)) {
+        if (valuea[0] === search)
+          for (const valueb of Object.entries(valuea[1])) {
+            // valueb[0] is the unique key
+            let att = {
+              g: 0,
+              i: 0,
+            };
+            for (const [key, val] of Object.entries(attendeesData)) {
+              if (key === valueb[0]) {
+                for (const x of Object.entries(val)) {
+                  if (x[0] === "i") {
+                    att.i = Object.entries(x[1]).length;
+                  } else {
+                    att.g = Object.entries(x[1]).length;
+                  }
+                }
+              }
+            }
+
+            temp.push({
+              0: valueb[0],
+              1: (
+                <Event
+                  date={valueb[1].eventDate}
+                  name={valueb[1].eventTitle}
+                  description={valueb[1].eventDesc}
+                  ammountGoing={att.g}
+                  ammountInterested={att.i}
+                  url={valueb[1].eventUrl}
+                  id={valueb[0]}
+                ></Event>
+              ),
+            });
+          }
       }
       let wrapper = (
         <ul className="eventsGrid">
@@ -88,7 +164,15 @@ export default function Feed({ onClick }: FeedProps) {
 
   return (
     <Wrapper onClick={onClick} cssClass="home">
-      {structureEvents(events, attendees)}
+      <label htmlFor="date">Choose a date to filter the events</label>
+      <input
+        type="date"
+        name="search"
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
+      {search
+        ? eventsForDate(events, attendees)
+        : structureEvents(events, attendees)}
     </Wrapper>
   );
 }
